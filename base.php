43c178d99fa432b13bd84c1f59d7b51b44b5838e
Franckensteyn <y.assiine@hotmail.fr>
2017-05-05 14:02:38 +0200

    lulu

--- base.php
+++ base.php
@@ -1,3 +1,21 @@
+<?php
+/*if(!strstr($_SERVER['PHP_SELF'],"index.php")){
+    header("Location: ./");
+}*/
+
+// requête pour récupérer les messages des utilisateurs.
+$sql = "SELECT u.idutil, u.login, m.ladate, m.texte, m.titre 
+    FROM util u 
+    INNER JOIN message m
+        ON u.idutil = m.util_idutil
+   ";
+$recup_sql = mysqli_query($db, $sql)or die(mysqli_error($db));
+/*if(!mysqli_num_rows($recup_sql)){
+    $erreur = "Article introuvable!";
+}else{*/
+     $ligne = mysqli_fetch_assoc($recup_sql);
+/*}*/
+?>
 <!DOCTYPE html>
     <html>
     <head>
@@ -8,9 +26,17 @@
     <body>
     <!-- bordure du haut avec le nom des utilisateur connecté-->
     <div class="menu">
-            <div class="back"><i class="fa fa-chevron-left"></i> <img src="http://i.imgur.com/DY6gND0.png" draggable="false"/></div>
-            <div class="name">Houssain</div>
-            <div class="last">18:09</div>
+            <?php
+           
+            if ($_SESSION['idutil'] == $ligne['idutil']) {
+                       $a = 'other';
+            }else{
+                           $a='self';
+                       }
+            ?>
+    <div class="back"><i class="fa fa-chevron-left"></i> <img src="http://i.imgur.com/DY6gND0.png" draggable="false"/></div>
+            <div class="name"><?=$ligne['idutil']?></div>
+            <div class="last"><?=$ligne['ladate']?></div>
         </div>
     <!-- le contenue du tchat-->
     <ol class="chat">
@@ -19,38 +45,28 @@
         <div class="avatar"><img src="http://i.imgur.com/DY6gND0.png" draggable="false"/></div>
       
       <div class="msg">
-        <p id="colorenvoie">Houssain : </p>
-        <p>Fayn!</p>
-        <p>trkl ou quoi ?  <emoji class="pizza"/></p>
-        <time>20:17</time>
+        <p id="colorenvoie"><?=$ligne['idutil']?> :</p>
+        <p><?=$ligne['titre']?></p>
+        <p><?=$ligne['texte']?></p>
+        <time><?=$ligne['ladate']?></time>
       </div>
     </li>
 <!-- div du message de réponse -->
     <li class="self">
         <div class="avatar"><img src="http://i.imgur.com/HYcn9xO.png" draggable="false"/></div>
       <div class="msg">
-        <p id="colorreceveur">Yassine : </p>
-        <p>Yo</p>
-        <p>A FOU  tu te souviens de vendredi ?? <emoji class="books"/></p>
-        <p>quand on étais en classe ? </p>
-        <time>20:18</time>
+        <p id="colorreceveur"><?=$ligne['idutil']?> :</p>
+        <p><?=$ligne['titre']?></p>
+        <p><?=$ligne['texte']?></p>
+        <time><?=$ligne['ladate']?></time>
       </div>
     </li>
     <!-- div du message envoyé -->
-    <li class="other">
-        <div class="avatar"><img src="http://i.imgur.com/DY6gND0.png" draggable="false"/></div>
-      
-      <div class="msg">
-        <p id="colorenvoie">Houssain : </p>
-        <p>Hahahahahaha</p>
-        <p>Ouais truc de fou !<emoji class="pizza"/></p>
-        <time>20:17</time>
-      </div>
-    </li>
     
     </ol>
-    <!-- fin du contenue du tchat -->
+    <!-- fin du contenue tu tchat -->
     <!-- bouton d'envoie-->
     <input class="textarea" type="text" placeholder="Type here!"/><div class="emojis"></div>
+    <?php  ?>
     </body>
     </html>
--- connect.php
+++ connect.php
@@ -1,8 +1,13 @@
 <?php
-	require_once 'config.php';
 
-	$db = mysqli_connect(DB_HOST,DB_LOGIN,DB_PASS,DB_NAME)
-	OR die("Echec de la connexion: ".utf8_encode(mysqli_connect_error())."<br/>Num&eacute;ro d'erreur".utf8_encode(mysqli_connect_errno()));
+require_once 'config.php';
 
-	mysqli_set_charset($db,DB_CHARSET);
-?>
+/* 
+ * Connexion en procédural en mysqli
+ */
+
+$db = @mysqli_connect(DB_HOST,DB_LOGIN,DB_PWD,DB_NAME) 
+        // en cas d'échec
+        or die("Echec de la connexion: ". utf8_encode(mysqli_connect_error())."<br/>Num&eacute;ro d'erreur: ". mysqli_connect_errno());
+
+mysqli_set_charset($db, DB_CHARSET);
