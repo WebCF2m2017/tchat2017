a2bf553811106a33c599e3ea1a40d7554d8c2e1c
michaeljpitz <michaeljpitz@gmail.com>
2017-05-19 15:30:08 +0200

    corrections anciens fichiers

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once 'config.php';
-
-	$db = mysqli_connect(DB_HOST,DB_LOGIN,DB_PASS,DB_NAME)
-	OR die("Echec de la connexion: ".utf8_encode(mysqli_connect_error())."<br/>Num&eacute;ro d'erreur".utf8_encode(mysqli_connect_errno()));
-
-	mysqli_set_charset($db,DB_CHARSET);
-?>
